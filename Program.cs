be20776d62aa7e06fadf2bec78b16fd2623ae8da
shuabruh <shuabruh@student.telkomuniversity.ac.id>
2024-03-18 01:26:45 +0700

    Menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -2,6 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataGeneric<string> genericData = new DataGeneric<string>("1302220072");
+        genericData.printData();
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah " + data);
     }
 }
